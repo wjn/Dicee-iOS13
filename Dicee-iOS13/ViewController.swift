011261899e0f9f3337079da38ef77a3db51ed445
wjn <blitzes.toll.0n@icloud.com>
2022-04-23 18:32:53 -0500

    randomized dice with button press

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -13,16 +13,34 @@ class ViewController: UIViewController {
     // IBOutlet allow reference of UI element.
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
+    @IBOutlet weak var diceMessageLabel: UILabel!
+    
+    var diceArray = [ #imageLiteral(resourceName: "DiceOne") , #imageLiteral(resourceName: "DiceTwo") , #imageLiteral(resourceName: "DiceThree") , #imageLiteral(resourceName: "DiceFour") , #imageLiteral(resourceName: "DiceFive") , #imageLiteral(resourceName: "DiceSix") ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // using #imageLiteral() for start images.
-        // who          what    value
-        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
-        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
     }
 
 
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        print("rollButtonPressed")
+        let leftIndex = Int.random(in:0...5)
+        let rightIndex = Int.random(in:0...5)
+        let diceSum = leftIndex + rightIndex + 2
+        var isRollDouble = false
+        if ( leftIndex == rightIndex ) {
+            isRollDouble = true
+        }
+        var diceMsg = "\(diceSum), nice roll!"
+        if ( isRollDouble ) {
+            diceMsg = "DOUBLE for \(diceSum)!"
+        }
+        
+        diceImageView1.image = diceArray[leftIndex]
+        diceImageView2.image = diceArray[rightIndex]
+        
+        diceMessageLabel.text = diceMsg
+    }
 }
 
